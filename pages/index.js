ee7c9a11a3c8ed179811e2ea6f25be285699c161
yruben <y.ruben1901@gmail.com>
2021-01-18 22:51:52 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Ingeniero en Ciencias Informáticas con 10+ años de experiencia en el desarrollo de software empleando varias metodologías.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps(){
+  const allPostsData = await getSortedPostsData()
+  return{
+    props:{
+      allPostsData
+    }
+  }
+}
